df0935139c7c2ca601ef4c553d510efac6fc7973
AnthonyBianco <anthonysilvanobianco@gmail.com>
2019-09-24 21:53:09 +0000

    working with John

---
+++ spinner2.js
@@ -0,0 +1,9 @@
+const spinning = ['\r|', '\r/', '\r-', "\r\\",'\r|', '\r/', '\r-', "\r\\"];
+let time = 0;
+let count = 0;
+for (let i = 0; i < spinning.length; i++){
+  setTimeout(() => { 
+    process.stdout.write(spinning[i]);
+  }, time += 200);
+  
+}
